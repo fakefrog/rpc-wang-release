a47f69c693f0199d5ef6ef17dc92d91ae1200ad6
baiyundou <qwe6588038>
2020-06-18 19:24:19 +0800

    郭源潮

---
+++ rpc-starter/src/main/java/com/wang/starter/rpc/config/AbstractClient.java
@@ -0,0 +1,12 @@
+package com.wang.starter.rpc.config;
+
+/**
+ * <p>Package:com.wang.starter.rpc.config</p>
+ * <p>Description: </p>
+ * <p>Company: com.2dfire</p>
+ *
+ * @author baiyundou
+ * @date 2020/6/18 19:21
+ */
+public class AbstractClient {
+}
